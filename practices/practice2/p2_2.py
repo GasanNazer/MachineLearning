105c46c678874fa65fffcb7b61880bc8c617d5e7
RoNiTo0 <veronikayankova3@gmail.com>
2020-08-20 21:34:12 +0300

    Add files via upload

--- practices/practice2/p2_2.py
+++ practices/practice2/p2_2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize as opt
 from pandas.io.parsers import read_csv
+from sklearn.preprocessing import PolynomialFeatures
 
 def carga_csv(file_name):
     """carga el fichero csv especificado y lo
@@ -11,7 +12,7 @@ def carga_csv(file_name):
     # suponemos que siempre trabajaremos con float
     return valores.astype(float)
 
-datos = carga_csv('ex2data1.csv')
+datos = carga_csv('ex2data2.csv')
 X = datos[:, :-1] 
 Y = datos[:, -1] 
 m = np.shape(X)[0]
@@ -36,9 +37,12 @@ def pinta_frontera_recta(X, Y, theta):
     np.linspace(x2_min, x2_max))
 
     theta = theta.reshape((len(theta), 1))
-    h = sigmoid(np.matmul(np.c_[np.ones((xx1.ravel().shape[0], 1)),
-    xx1.ravel(),
-    xx2.ravel()], theta))
+
+    trans = PolynomialFeatures(degree = 6, interaction_only=False, include_bias=True)
+    x = np.c_[xx1.ravel(), xx2.ravel()]
+    X = trans.fit_transform(x)
+    print(X.shape)
+    h = sigmoid(np.matmul(X, theta))
     h = h.reshape(xx1.shape)
 
     # el cuarto parĂ¡metro es el valor de z cuya frontera se
@@ -48,29 +52,32 @@ def pinta_frontera_recta(X, Y, theta):
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def cost(theta, X, Y):
+def cost(theta, X, Y, lambd = 1):
     theta = theta.reshape((len(theta), 1))
     Y = Y.reshape((len(Y), 1))   
     A = sigmoid(np.matmul(X, theta))
-    return (- 1 / (len(X))) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+    reg = (lambd / (2 * m)) * np.sum(theta ** 2)
+    return (- 1 / (len(X))) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A)) + reg
 
 
-def gradient(theta, X, Y):
+def gradient(theta, X, Y, lambd = 1):
     theta = theta.reshape((len(theta), 1))
     Y = Y.reshape((len(Y), 1))
     A = sigmoid(np.matmul(X, theta))
-    return np.dot(X.T, (A - Y)) / len(Y)
+    identity = np.identity(theta.shape[0])
+    identity[0][0] = 0
+    return np.dot(X.T, (A - Y)) / len(Y) + (lambd / m) * np.dot(identity, theta)
 
 
 def model(x, Y):
-    X = np.hstack([np.ones([m, 1]), x])
+    trans = PolynomialFeatures(degree = 6, interaction_only=False, include_bias=True)
+    X = trans.fit_transform(x)
+    
     theta = np.zeros(X.shape[1])
     result = opt.fmin_tnc (func=cost, x0=theta, fprime=gradient, args = (X, Y))
     theta = result[0]
-    print('cost: ' + str(cost(result[0], X, Y)))
     pinta_frontera_recta(x, Y, theta)
     dibuja_casos(x, Y)
 
 model(X, Y)
 plt.show()
-
